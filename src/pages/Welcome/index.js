a66d719660cfb64c467050bc0f4c6ab9d62a6643
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-05-12 12:43:54 +0100

    updated logo

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import FrontPageLogo from "../../images/GitHub-Logo.png"
 
 const Welcome = () => (
     <>
     <h1>Check your repositories here</h1>
     <h3>The bootleg GitHub at your service</h3>
-    <img style={{ maxHeight: "70vh", maxWidth: "80vw" }} src="../../images/GitHub-Logo.png" alt="GitHubLogo" />
+    <img style={{ maxHeight: "70vh", maxWidth: "80vw" }} src={FrontPageLogo} alt="GitHubLogo" />
     <br></br>
-    <p>Made with a lot of effor by Andrew and Jianli, with love, we want to sleep.</p>
+    <p>Made with a lot of effort by Andrew and Jianli, with love, we want to sleep.</p>
 
     </>
 )
